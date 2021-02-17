3452075f04b20f7a1991b6cf36c0888d5cc55924
Stepan Anchugov <kixxx1@gmail.com>
2021-02-17 21:42:04 +0500

    Update project.clj with description and GitHub URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-test-task "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-test-task "0.1.0"
+  :description "Dictionary attacker"
+  :url "https://github.com/kix/dict-match"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
