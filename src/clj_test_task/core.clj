993c041f9f86a9d2be43d9041a9658bb581311f0
Stepan Anchugov <kixxx1@gmail.com>
2021-02-17 21:40:01 +0500

    Add a check for dict file existence

--- src/clj_test_task/core.clj
+++ src/clj_test_task/core.clj
@@ -1,10 +1,14 @@
 (ns clj-test-task.core
   (:require [clojure.java.io :as io]))
 
-(defn try-fn [_]
+(defn try-fn 
+  "Mock function. Randomly returns true or false for now."
+  [_]
   (== 1 (rand-int 2)))
 
-(defn dictionary-attack [try-fn dict-path out-path]
+(defn dictionary-attack 
+  "Puts lines from dict-path that match try-fn to out-path"
+  [try-fn dict-path out-path]
   (with-open [r (io/reader dict-path)
               w (io/writer out-path)]
     (doseq [line (line-seq r)]
@@ -12,9 +16,12 @@
         (.write w (str line "\n"))))))
 
 (defn -main
-  "Runs a dictionary attack given a dictionary file as first argument"
+  "Runs a dictionary attack given a dictionary file"
   [& [dict-path out-path]]
   (let [out-path (or out-path "out.txt")]
-    (println "Using" dict-path "as dictionary, outputting matches to" out-path)
-    (dictionary-attack try-fn dict-path out-path)
-    (println "Done!")))
+    (if (.exists (io/file dict-path))
+      (do
+        (println "Using" dict-path "as dictionary, outputting matches to" out-path)
+        (dictionary-attack try-fn dict-path out-path)
+        (println "Done!"))))
+      (println "Dict does not exist at" dict-path))
