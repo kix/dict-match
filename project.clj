07af45b63e3379a1d0e1021939449e11a9801921
Stepan Anchugov <kixxx1@gmail.com>
2021-02-18 15:58:30 +0500

    Core.async solution

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/kix/dict-match"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :profiles {:dev {:dependencies [[proto-repl "0.3.1"]]}}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "1.3.610"]]
+  :profiles {:dev {:dependencies [[proto-repl "0.3.1"]
+                                  [criterium "0.4.6"]]}}
   :repl-options {:init-ns clj-test-task.core}
   :main clj-test-task.core)
--- src/clj_test_task/core.clj
+++ src/clj_test_task/core.clj
@@ -1,27 +1,33 @@
 (ns clj-test-task.core
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.core.async :refer [chan <! put! go go-loop]]))
 
-(defn try-fn 
+(defn try-fn
   "Mock function. Randomly returns true or false for now."
   [_]
-  (== 1 (rand-int 2)))
+  (= 1 (rand-int 2)))
 
-(defn dictionary-attack 
+(defn dictionary-attack
   "Puts lines from dict-path that match try-fn to out-path"
   [try-fn dict-path out-path]
-  (with-open [r (io/reader dict-path)
-              w (io/writer out-path)]
-    (doseq [line (line-seq r)]
-      (when (try-fn line)
-        (.write w (str line "\n"))))))
+  (let [ch (chan)]
+   (with-open [r (io/reader dict-path)]
+     (doall (pmap
+             #(put! ch %)
+             (line-seq r))))
+   (go-loop []
+     (when-let [password (<! ch)]
+       (when (try-fn password)
+         (spit out-path (str password "\n") :append true))
+       (recur)))))
 
 (defn -main
   "Runs a dictionary attack given a dictionary file"
   [& [dict-path out-path]]
   (let [out-path (or out-path "out.txt")]
+    (io/delete-file out-path)
     (if (.exists (io/file dict-path))
       (do
         (println "Using" dict-path "as dictionary, outputting matches to" out-path)
-        (dictionary-attack try-fn dict-path out-path)
-        (println "Done!"))))
-      (println "Dict does not exist at" dict-path))
+        (dictionary-attack try-fn dict-path out-path))
+      (println "Dict does not exist at" dict-path))))
